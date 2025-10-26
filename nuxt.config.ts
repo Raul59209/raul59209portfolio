3b8704e99ae24723d4cfbcbc69946efd2c7b471c
Raul59209 <raul.gonzalez@epitech.eu>
2025-10-26 20:52:25 +0000

    all working now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+ssr: false,
+  app: {
+    baseURL: '/raul59209portfolio/', // Replace with your actual GitHub repo name
+  },
+  nitro: {
+    preset: 'static'
+  },
   modules: ['@nuxtjs/tailwindcss'],
     css: ['@/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
